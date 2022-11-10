438364ba7885a7b5568a9eb6138ed2d5e9c5e1ab
Scott Clements <clemencyoflee@gmail.com>
2022-11-09 16:11:09 -0800

    Imported unittest to run a comprehensive test on the future to-do app

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install working successfully!' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # User can access online to-do app
+        # This accesses the homepage
+        self.browser.get('http://localhost:8000')
+
+
+        # This should be what the user sees in the page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # User should be able to add a to-do list item into a text box immediately
+        # And that item will show up as:
+        # "1: Item 1"
+
+        # The text box will then reappear below the first item
+
+        # The site will remember the user's list by generating a unique URL 
+        # (there will also be explanatory text as to how this is done)
+
+        # User should be able to revisit the site and see the to-do list
+
+
+if __name__ == "__main__":
+    unittest.main()
