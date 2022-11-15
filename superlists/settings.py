509a9575460bb7f2ba5a4627ade4c09f472fa635
Scott Clements <clemencyoflee@gmail.com>
2022-11-15 15:55:06 -0800

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--^1_*l$t01ub+peu@jpic8y8_0+rz(j3meg+arw^5og8!ld(+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
