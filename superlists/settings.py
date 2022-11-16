0a93f0c502faf2987afb8c4c501911f5a5233dfa
Scott Clements <clemencyoflee@gmail.com>
2022-11-15 17:30:13 -0800

    Updated settings to allow for CSRF form submissions.

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://staging.testingsuperlist.online", "http://staging.testingsuperlist.online"]
 
 
 # Application definition
