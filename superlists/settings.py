c4e48438b9e312248bda373c7d531f57d81251e0
Scott Clements <clemencyoflee@gmail.com>
2022-11-15 19:04:12 -0800

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--^1_*l$t01ub+peu@jpic8y8_0+rz(j3meg+arw^5og8!ld(+6'
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environn['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'django-insecure--^1_*l$t01ub+peu@jpic8y8_0+rz(j3meg+arw^5og8!ld(+6'
+    ALLOWED_HOSTS = []
+
+# SECRET_KEY = 'django-insecure--^1_*l$t01ub+peu@jpic8y8_0+rz(j3meg+arw^5og8!ld(+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ["https://staging.testingsuperlist.online", "http://staging.testingsuperlist.online"]
 
