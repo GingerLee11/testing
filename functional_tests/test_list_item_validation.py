0664a259a5f3eacb102c0414b41b0d04c91491c2
Scott Clements <clemencyoflee@gmail.com>
2022-11-17 15:40:18 -0800

    Added helper function in base.py, and filled out test for item validation.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,14 +13,36 @@ class ItemValidationTest(FunctionalTest):
         # Akane goes to the home page and accidentally tries to submit an empty list item.
         
         # She hits Enter on the empty input box.
-        
+        self.browser.get(self.live_server_url)
+
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+
         # The home page refreshes, and there is an error message saying that list items 
         # cannot be blank
-
+        self.wait_for(
+            lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+            )
+        )
         # She tries again with some text for the item, which now works
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Look for fish')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Look for fish')
+
         # Perversely, she now decided to submit a second blank list item
-        
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+                
         # She receives a similar warning on the list page
+        self.wait_for(
+            lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+            )
+        )
 
         # And she can correct it by filling some text in
-        self.fail('Write me!')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Try to catch fish without getting wet')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Look for fish')
+        self.wait_for_row_in_list_table('2: Try to catch fish without getting wet')
