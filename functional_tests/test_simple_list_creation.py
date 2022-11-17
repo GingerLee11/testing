139661b05ad4c6a10aa45c6a173d4c4709e1c43f
Scott Clements <clemencyoflee@gmail.com>
2022-11-17 15:13:49 -0800

    Moved FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,41 +1,11 @@
-from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
 
-import time
-import unittest
-import os
-
-MAX_WAIT = 10
+from .base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = f'http://{staging_server}' 
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -71,10 +41,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # The user will then enter in "Stat monster for Stonetop"
         self.wait_for_row_in_list_table('2: Stat monster for Stonetop')
         self.wait_for_row_in_list_table('1: Prep for Stonetop campaign')
-
         # The site will remember the user's list by generating a unique URL 
         # (there will also be explanatory text as to how this is done)
-
         # User should be able to revisit the site and see the to-do list
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -119,26 +87,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Prep for Stonetop campaign', page_text)
         self.assertIn('Pick new move for Isra', page_text)
-
-    def test_layout_and_styling(self):
-        # The user goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # They notice the input box is nicely centered
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 
-            512,
-            delta=10,
-        )
-        # They start a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 
-            512,
-            delta=10,
-        )
