70a822e78f3badc0ab9420d5597383314d432132
Scott Clements <clemencyoflee@gmail.com>
2022-11-18 13:40:59 -0800

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -44,3 +44,20 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Look for fish')
         self.wait_for_row_in_list_table('2: Try to catch fish without getting wet')
+
+    def test_cannot_add_duplicate_items(self):
+        # Akane goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Eat Fish')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Eat Fish')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Eat Fish')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # She sees a helpul error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error'),
+            "You've already got this in your list"
+        ))
