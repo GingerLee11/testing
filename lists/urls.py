96dedebf5a1255a1452bd7b984f5a3ca5db11f41
Scott Clements <clemencyoflee@gmail.com>
2022-11-17 18:02:20 -0800

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('new', views.new_list, name='new-list'),
     re_path(r'^(\d+)/$', views.view_list, name='view-list'),
-    re_path(r'^(\d+)/add_item$', views.add_item, name='add-item'),
 ]
