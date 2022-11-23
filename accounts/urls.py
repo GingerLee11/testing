61846b6fb5445a1710977ec5df9f65a36b736fa7
Scott Clements <clemencyoflee@gmail.com>
2022-11-23 14:08:12 -0800

    Tested and added functionality to logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-
+from django.contrib.auth.views import LogoutView
 from accounts import views
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send-login-email'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- superlists/settings.py
+++ superlists/settings.py
@@ -153,3 +153,7 @@ EMAIL_HOST_USER = 'testingwebsites11@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# Redirects for logout
+# LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
